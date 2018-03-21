80f5bb0a2efe92584b87085f4f3d444535b96835
Adriana C and julia sheremet <julia@>
2018-03-21 15:51:49 -0700

    impliment different form input types

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-  $("form#booking-form").submit(function(event) {
+  $("form#food-survey").submit(function(event) {
+    event.preventDefault();
+    var question1input = $("#date").val();
+    var question2input = $("#beverage").val();
+    var question3input = $("input:radio[name=flavor]:checked").val();
+    var question4input = $("#color").val();
+
+    $(".container").html("Your answers as following: </br><p>"+question1input+"</p></br><p>"+question2input+"</p><p>"+question3input+"</p><p>"+question4input+"</p>");
 
   });
 });
